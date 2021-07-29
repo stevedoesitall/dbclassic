fc165c98ecfdaa1156ce392bc25aacf4fd241e68
Steve Giordano <stephenagiordano@gmail.com>
2021-07-28 23:59:23 -0400

    Updating knexfile connection

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,33 @@
-import { prodCreds, stagingCreds, devCreds } from "./config/db-creds.js"
+import dotenv from "dotenv"
+dotenv.config()
 
 const knexfile =  {
 	development: {
 		client: "postgresql",
-		connection: devCreds
+		connection: {
+			host: process.env.DB_HOST,
+			database: process.env.DB_NAME,
+			user: process.env.DB_USER,
+			password: process.env.DB_PASS,
+			port: 5432,
+			ssl: { 
+				rejectUnauthorized: false
+			}
+		},
 	},
 
 	staging: {
 		client: "postgresql",
-		connection: stagingCreds,
+		connection: {
+			host: process.env.DB_HOST,
+			database: process.env.DB_NAME,
+			user: process.env.DB_USER,
+			password: process.env.DB_PASS,
+			port: 5432,
+			ssl: { 
+				rejectUnauthorized: false
+			}
+		},
 		pool: {
 			min: 2,
 			max: 10
@@ -21,10 +40,10 @@ const knexfile =  {
 	production: {
 		client: "postgresql",
 		connection: {
-			host: process.env.DB_HOST || prodCreds.host,
-			database: process.env.DB_NAME || prodCreds.database,
-			user: process.env.DB_USER || prodCreds.user,
-			password: process.env.DB_PASS || prodCreds.password,
+			host: process.env.DB_HOST,
+			database: process.env.DB_NAME,
+			user: process.env.DB_USER,
+			password: process.env.DB_PASS,
 			port: 5432,
 			ssl: { 
 				rejectUnauthorized: false
