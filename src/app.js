9301ca218e6354cac4046389cc78083704adda12
Steve Giordano <stephenagiordano@gmail.com>
2021-07-30 19:39:52 -0400

    Committing

--- src/app.js
+++ src/app.js
@@ -60,7 +60,7 @@ app.get("/date/:date", async (req, res) => {
 	const { data, prevDate, nextDate } = await getDate(date)
 
 	const dateTime = new Date(date).getTime()
-	const formattedDate = new Date(dateTime + 86400000).toDateString()
+	const formattedDate = new Date(dateTime).toDateString()
 
 	if (!data) {
 		return res.render("error")
