5bc06bcd6a24ac0467f5e6418c59df5654dcbbcd
Steve Giordano <stephenagiordano@gmail.com>
2021-08-01 22:35:56 -0400

    Committing

--- public/scripts/utils/set-cookie.js
+++
@@ -1,7 +0,0 @@
-//NOTE: Bundle these all together!
-
-const setCookie = () => {
-	document.cookie = "momus_id=110ec58a-a0f2-4ac4-8393-c866d813b8d1"
-}
-
-setCookie()
--- src/app.js
+++ src/app.js
@@ -33,6 +33,11 @@ app.use("/tweets", tweetsRouter)
 
 app.get("", async (req, res) => {
 	const { allDates } = await getTweets()
+	console.log(req.cookies)
+	res.cookie("momus_id", "110ec58a-a0f2-4ac4-8393-c866d813b8d1", {
+		"sameSite": "strict",
+		"httpOnly": true
+	})
 
 	res.render("index", {
 		title: "Dadboner Classic",
