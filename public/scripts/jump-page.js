901f8c8c2b3c40ab11f779253f44e08c4bd04fd6
Steve Giordano <stephenagiordano@gmail.com>
2021-08-16 22:41:01 -0400

    Simple back to top button. Need to QA on phone/tablet

--- public/scripts/jump-page.js
+++ public/scripts/jump-page.js
@@ -1,6 +1,17 @@
 const selectedYear = document.querySelector("#year-select")
-
+const backToTopBtn = document.querySelector("#back-to-top-button")
 const jumpBtn = document.querySelector("#jump-button")
+
 jumpBtn.addEventListener("click", () => {
 	jumpBtn.setAttribute("href", `#day-${selectedYear.value}`)
+})
+
+document.addEventListener("scroll", () => {
+	const LIMIT = 400
+	const position = window.scrollY
+	if (position > LIMIT) {
+		backToTopBtn.classList.remove("hidden")
+	} else {
+		backToTopBtn.classList.add("hidden")
+	}
 })
