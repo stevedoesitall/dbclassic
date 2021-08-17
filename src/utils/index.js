4b3f58bc35535f1c118ece8018040737dd58e0d9
Steve Giordano <stephenagiordano@gmail.com>
2021-08-17 15:12:54 -0400

    Removed a bunch of dead code

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,23 +1,7 @@
-import getTweets from "./helpers/create-lists.js"
 import insertTweets from "./helpers/insert-tweets.js"
-import {
-	formatDateISO,
-	formatDateStr,
-	formatTime
-} from "./helpers/format-date-time.js"
-import { getTweetById, getTweetByDate } from "./helpers/get-tweets-lists.js"
-import { getUser, updateUser } from "./helpers/get-update-users.js"
 
 const _ = {
-	getTweets,
-	formatDateISO,
-	formatDateStr,
-	formatTime,
-	getTweetById,
-	getTweetByDate,
-	getUser,
-	insertTweets,
-	updateUser
+	insertTweets
 }
 
 export default _
