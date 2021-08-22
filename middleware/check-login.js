205a731e6a314c7640e0c2f9fcf104f0d720f023
Steve Giordano <stephenagiordano@gmail.com>
2021-08-21 23:14:53 -0400

    Committing

--- middleware/check-login.js
+++ middleware/check-login.js
@@ -6,16 +6,14 @@ const checkLogin = async (req, res, next) => {
 	if (userCookies.momus_id) {
 		const user = new User()
 		const data = await user.fetchById(userCookies.momus_id)
-		
 		const { result } = data
-		
-		req.session.loggedIn = result.logged_in ? true : false
-		req.session.lastPageview =
-		result.last_pageview && result.logged_in ? result.last_pageview : false
-		req.session.loginId = userCookies.momus_id
 
+		req.session.loggedIn = result.logged_in ? true : false
+		req.session.lastPageview = (result.last_pageview && result.logged_in) ? result.last_pageview : false
+		req.session.loginId = userCookies.momus_id
 	} else {
 		req.session.loggedIn = false
+		req.session.lastPageview = false
 	}
 
 	next()
--- middleware/set-cookie.js
+++ middleware/set-cookie.js
@@ -1,15 +1,19 @@
 const setCookie = (req, res, next) => {
-	const isLoggedIn = req.body.updates.loggedIn
-
-	if (isLoggedIn) {
-		res.cookie("momus_id", req.body.id, {
-			sameSite: "strict",
-			httpOnly: true
-		})
-	} else {
-		res.clearCookie("momus_id")
+	const loginAction = Object.keys(req.body.updates).includes("loggedIn")
+	const cookieObj = {
+		sameSite: "strict",
+		httpOnly: true
 	}
 
+	if (loginAction) {
+		const isLoggedIn = req.body.updates.loggedIn
+		if (isLoggedIn) {
+			res.cookie("momus_id", req.body.id, cookieObj)
+		} else {
+			res.clearCookie("momus_id")
+		}
+	}
+	
 	next()
 }
 
