4806c8df22e7c39fb8c5dff02b5501d654d16d77
Steve Giordano <stephenagiordano@gmail.com>
2022-07-15 23:27:28 -0400

    Attemping link fix

--- utils/get-dll.js
+++ utils/get-dll.js
@@ -4,15 +4,13 @@ const getLinkedTweets = (allTweets, type, value) => {
 	const tweetValues = []
 
 	allTweets.forEach(tweet => {
-		const tweetValue = type === "date" ? formatDateISO(tweet.date) : tweet[type]
+		const tweetValue = type === "date" ? formatDateISO(tweet.created_at) : tweet[type]
 		!tweetValues.includes(tweetValue) ? tweetValues.push(tweetValue) : null
 	})
 
-	const tweetValuesSorted = tweetValues.sort((a, b) => a.id - b.id)
-
-	const index = tweetValuesSorted.indexOf(value)
-	const prev = index > 0 ? tweetValuesSorted[index - 1] : null
-	const next = index < tweetValuesSorted.length - 1 ? tweetValuesSorted[index + 1] : null
+	const index = tweetValues.indexOf(value)
+	const prev = index > 0 ? tweetValues[index - 1] : null
+	const next = index < tweetValues.length - 1 ? tweetValues[index + 1] : null
 
 	return {
 		prev,
